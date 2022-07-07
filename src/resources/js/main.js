ce63e4cb0907238da865e6f079f54a267928d9dd
Renan Breno <renan.breno@fmd.ag>
2022-07-07 17:05:18 -0300

    fix(masks): Adiciona máscaras após document ready

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -12,51 +12,55 @@ function getUserBrowser() {
   }
 }
 
-function setMask(elements, maskOptions) {
-  elements.forEach(function (element) {
-    const mask = IMask(element, maskOptions);
+function setupMasks() {
+  function setMask(elements, maskOptions) {
+    elements.forEach(function (element) {
+      const mask = IMask(element, maskOptions);
 
-    mask.on('complete', function () {
-      // Safari doesn't detect the latest input changes
-      if (getUserBrowser() === 'safari') {
-        element.dispatchEvent(new InputEvent('change'));
-      }
+      mask.on('complete', function () {
+        // Safari doesn't detect the latest input changes
+        if (getUserBrowser() === 'safari') {
+          element.dispatchEvent(new InputEvent('change'));
+        }
+      });
     });
-  });
+  }
+
+  const phoneMaskOptions = {
+    mask: [
+      { mask: '(00) 0000-0000' },
+      { mask: '(00) 00000-0000' },
+    ],
+  };
+
+  setMask(
+      document.querySelectorAll('.wa-chat-mask-phone'),
+      phoneMaskOptions,
+  );
+
+  const cpfMaskOptions = { mask: '000.000.000-00' };
+
+  setMask(
+      document.querySelectorAll('.wa-chat-mask-cpf'),
+      cpfMaskOptions,
+  );
+
+  const cnpjMaskOptions = { mask: '00.000.000/0000-00' };
+
+  setMask(
+      document.querySelectorAll('.wa-chat-mask-cnpj'),
+      cnpjMaskOptions,
+  );
+
+  const cpfcnpjMaskOptions = { mask: [cpfMaskOptions, cnpjMaskOptions] };
+
+  setMask(
+      document.querySelectorAll('.wa-chat-mask-cpfcnpj'),
+      cpfcnpjMaskOptions,
+  );
 }
 
-const phoneMaskOptions = {
-  mask: [
-    { mask: '(00) 0000-0000' },
-    { mask: '(00) 00000-0000' },
-  ],
-};
-
-setMask(
-    document.querySelectorAll('.wa-chat-mask-phone'),
-    phoneMaskOptions,
-);
-
-const cpfMaskOptions = { mask: '000.000.000-00' };
-
-setMask(
-    document.querySelectorAll('.wa-chat-mask-cpf'),
-    cpfMaskOptions,
-);
-
-const cnpjMaskOptions = { mask: '00.000.000/0000-00' };
-
-setMask(
-    document.querySelectorAll('.wa-chat-mask-cnpj'),
-    cnpjMaskOptions,
-);
-
-const cpfcnpjMaskOptions = { mask: [cpfMaskOptions, cnpjMaskOptions] };
-
-setMask(
-    document.querySelectorAll('.wa-chat-mask-cpfcnpj'),
-    cpfcnpjMaskOptions,
-);
+document.addEventListener("DOMContentLoaded", setupMasks);
 
 function whatsappChatSubmitted() {
   return new Promise(resolve => {
